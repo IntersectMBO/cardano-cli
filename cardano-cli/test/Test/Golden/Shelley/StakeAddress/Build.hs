IntersectMBO/cardano-node

Reorganize cardano-cli tests
Split a single test into a pair of tests (golden and cli) that are easier
to run separately. Also rearrange the module heirarchy so that the module
names indicate which test the module belongs to.

Apart from the reorg, the only other change is changing some tests from
sequential to parallel execution.

cardano-cli/test/Test/Golden/Shelley/StakeAddress/Build.hs
@@ -1,0 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Golden.Shelley.StakeAddress.Build
+  ( golden_shelleyStakeAddressBuild
+  ) where
+
+import           Cardano.Prelude
+import           Hedgehog (Property)
+import           Test.OptParse as OP
+
+{- HLINT ignore "Use camelCase" -}
+
+golden_shelleyStakeAddressBuild :: Property
+golden_shelleyStakeAddressBuild = propertyOnce . moduleWorkspace "tmp" $ \_ -> do
+  verificationKeyFile <- noteInputFile "test/Test/golden/shelley/keys/stake_keys/verification_key"
+  goldenRewardAddressFile <- noteInputFile "test/Test/golden/shelley/keys/stake_keys/reward_address"
+
+  rewardAddress <- execCardanoCLI
+    [ "shelley","stake-address","build"
+    , "--mainnet"
+    , "--staking-verification-key-file", verificationKeyFile
+    ]
+
+  goldenRewardsAddress <- OP.readFile goldenRewardAddressFile
+
+  equivalence rewardAddress goldenRewardsAddress

cardano-cli/test/Test/Golden/Shelley/StakeAddress/RegistrationCertificate.hs
@@ -1,0 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Golden.Shelley.StakeAddress.RegistrationCertificate
+  ( golden_shelleyStakeAddressRegistrationCertificate
+  ) where
+
+import           Cardano.Prelude
+import           Hedgehog (Property)
+import           Test.OptParse
+
+{- HLINT ignore "Use camelCase" -}
+
+golden_shelleyStakeAddressRegistrationCertificate :: Property
+golden_shelleyStakeAddressRegistrationCertificate = propertyOnce . moduleWorkspace "tmp" $ \tempDir -> do
+  keyGenStakingVerificationKeyFile <- noteInputFile "test/Test/golden/shelley/keys/stake_keys/verification_key"
+  registrationCertFile <- noteTempFile tempDir "registration.cert"
+
+  void $ execCardanoCLI
+    [ "shelley","stake-address","registration-certificate"
+    , "--staking-verification-key-file", keyGenStakingVerificationKeyFile
+    , "--out-file", registrationCertFile
+    ]
+
+  assertFileOccurences 1 "Stake Address Registration Certificate" registrationCertFile
+
+  assertEndsWithSingleNewline registrationCertFile

cardano-cli/test/Test/Golden/Shelley/TextEnvelope/Keys/GenesisDelegateKeys.hs
@@ -1,0 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Golden.Shelley.TextEnvelope.Keys.GenesisDelegateKeys
+  ( golden_shelleyGenesisDelegateKeys
+  ) where
+
+import           Cardano.Api.Typed (AsType (..), HasTextEnvelope (..))
+import           Cardano.Prelude
+import           Hedgehog (Property)
+import           Test.OptParse
+
+-- | 1. Generate a key pair & operational certificate counter file
+--   2. Check for the existence of the key pair & counter file
+--   3. Check the TextEnvelope serialization format has not changed.
+golden_shelleyGenesisDelegateKeys :: Property
+golden_shelleyGenesisDelegateKeys = propertyOnce . moduleWorkspace "tmp" $ \tempDir -> do
+  -- Reference keys
+  referenceVerKey <- noteInputFile "test/Test/golden/shelley/keys/genesis_delegate_keys/verification_key"
+  referenceSignKey <- noteInputFile "test/Test/golden/shelley/keys/genesis_delegate_keys/signing_key"
+  referenceOpCertCounter <- noteInputFile "test/Test/golden/shelley/keys/genesis_delegate_keys/operational_certificate_counter"
+
+  -- Key filepaths
+  verKey <- noteTempFile tempDir "genesis-delegate-verification-key-file"
+  signKey <- noteTempFile tempDir "genesis-delegate-signing-key-file"
+  opCertCounter <- noteTempFile tempDir "delegate-operational-cert-counter-file"
+
+  -- Generate payment verification key
+  void $ execCardanoCLI
+    [ "shelley","genesis","key-gen-delegate"
+    , "--verification-key-file", verKey
+    , "--signing-key-file", signKey
+    , "--operational-certificate-issue-counter-file", opCertCounter
+    ]
+
+  let signingKeyType = textEnvelopeType (AsSigningKey AsGenesisDelegateKey)
+      verificationKeyType = textEnvelopeType (AsVerificationKey AsGenesisDelegateKey)
+      operationalCertCounterType = textEnvelopeType AsOperationalCertificateIssueCounter
+
+  -- Check the newly created files have not deviated from the
+  -- golden files
+  checkTextEnvelopeFormat verificationKeyType referenceVerKey verKey
+  checkTextEnvelopeFormat signingKeyType referenceSignKey signKey
+  checkTextEnvelopeFormat operationalCertCounterType referenceOpCertCounter opCertCounter

cardano-cli/test/Test/Golden/Shelley/TextEnvelope/Keys/GenesisKeys.hs
@@ -1,0 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Golden.Shelley.TextEnvelope.Keys.GenesisKeys
+  ( golden_shelleyGenesisKeys
+  ) where
+
+import           Cardano.Api.Typed (AsType (..), HasTextEnvelope (..))
+import           Cardano.Prelude
+import           Hedgehog (Property)
+import           Test.OptParse
+
+-- | 1. Generate a key pair
+--   2. Check for the existence of the key pair
+--   3. Check the TextEnvelope serialization format has not changed
+golden_shelleyGenesisKeys :: Property
+golden_shelleyGenesisKeys = propertyOnce . moduleWorkspace "tmp" $ \tempDir -> do
+  -- Reference keys
+  referenceVerKey <- noteInputFile "test/Test/golden/shelley/keys/genesis_keys/verification_key"
+  referenceSignKey <- noteInputFile "test/Test/golden/shelley/keys/genesis_keys/signing_key"
+
+  -- Key filepaths
+  verKey <- noteTempFile tempDir "genesis-verification-key-file"
+  signKey <- noteTempFile tempDir "genesis-signing-key-file"
+
+  -- Generate payment verification key
+  void $ execCardanoCLI
+    [ "shelley","genesis","key-gen-genesis"
+    , "--verification-key-file", verKey
+    , "--signing-key-file", signKey
+    ]
+
+  let signingKeyType = textEnvelopeType (AsSigningKey AsGenesisKey)
+      verificationKeyType = textEnvelopeType (AsVerificationKey AsGenesisKey)
+
+  -- Check the newly created files have not deviated from the
+  -- golden files
+  checkTextEnvelopeFormat verificationKeyType referenceVerKey verKey
+  checkTextEnvelopeFormat signingKeyType referenceSignKey signKey

cardano-cli/test/Test/Golden/Shelley/TextEnvelope/Keys/VRFKeys.hs
@@ -1,0 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Golden.Shelley.TextEnvelope.Keys.VRFKeys
+  ( golden_shelleyVRFKeys
+  ) where
+
+import           Cardano.Api.Typed (AsType (..), HasTextEnvelope (..))
+import           Cardano.Prelude
+import           Hedgehog (Property)
+import           Test.OptParse
+
+-- | 1. Generate a key pair
+--   2. Check for the existence of the key pair
+--   3. Check the TextEnvelope serialization format has not changed.
+golden_shelleyVRFKeys :: Property
+golden_shelleyVRFKeys = propertyOnce . moduleWorkspace "tmp" $ \tempDir -> do
+  -- Reference keys
+  referenceVerKey <- noteInputFile "test/Test/golden/shelley/keys/vrf_keys/verification_key"
+  referenceSignKey <- noteInputFile "test/Test/golden/shelley/keys/vrf_keys/signing_key"
+
+  -- Key filepaths
+  verKey <- noteTempFile tempDir "vrf-verification-key-file"
+  signKey <- noteTempFile tempDir "vrf-signing-key-file"
+
+  -- Generate vrf verification key
+  void $ execCardanoCLI
+    [ "shelley","node","key-gen-VRF"
+    , "--verification-key-file", verKey
+    , "--signing-key-file", signKey
+    ]
+
+  let signingKeyType = textEnvelopeType (AsSigningKey AsVrfKey)
+      verificationKeyType = textEnvelopeType (AsVerificationKey AsVrfKey)
+
+  -- Check the newly created files have not deviated from the
+  -- golden files
+  checkTextEnvelopeFormat verificationKeyType referenceVerKey verKey
+  checkTextEnvelopeFormat signingKeyType referenceSignKey signKey

cardano-cli/test/Test/Golden/Shelley/TextView/DecodeCbor.hs
@@ -1,0 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Golden.Shelley.TextView.DecodeCbor
+  ( golden_shelleyTextViewDecodeCbor
+  ) where
+
+import           Cardano.Prelude
+import           Hedgehog (Property)
+import           Test.OptParse
+
+import qualified System.IO as IO
+
+{- HLINT ignore "Use camelCase" -}
+
+golden_shelleyTextViewDecodeCbor :: Property
+golden_shelleyTextViewDecodeCbor = propertyOnce $ moduleWorkspace "tmp" $ \tempDir -> do
+  unsignedTxFile <- noteInputFile "test/Test/golden/shelley/tx/unsigned.tx"
+  decodedTxtFile <- noteTempFile tempDir "decoded.txt"
+
+  -- Defaults to signing a Mainnet transaction.
+
+  decodedTxt <- execCardanoCLI
+    [ "shelley","text-view","decode-cbor"
+    , "--file", unsignedTxFile
+    ]
+
+  liftIO $ IO.writeFile decodedTxtFile decodedTxt
+
+  assertFileOccurences 1 "# int(4999998000)" decodedTxtFile
+  assertFileOccurences 1 "# int(2000)" decodedTxtFile
+  assertFileOccurences 1 "# int(1000)" decodedTxtFile
+
+  assertEndsWithSingleNewline decodedTxtFile
+  assertFileLines (>= 10) decodedTxtFile

cardano-cli/test/Test/Golden/Shelley/Transaction/Sign.hs
@@ -1,0 +1,65 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Golden.Shelley.Transaction.Sign
+  ( golden_shelleyTransactionSign
+  ) where
+
+import           Cardano.Prelude
+import           Hedgehog (Property)
+import           Test.OptParse
+
+{- HLINT ignore "Use camelCase" -}
+
+golden_shelleyTransactionSign :: Property
+golden_shelleyTransactionSign = propertyOnce $ moduleWorkspace "tmp" $ \tempDir -> do
+  txBodyFile <- noteInputFile "test/Test/golden/shelley/transaction-sign/tx-body-file"
+  initialUtxo1SigningKeyFile <- noteInputFile "test/Test/golden/shelley/transaction-sign/initial-utxo1.skey"
+  initialUtxo2SigningKeyFile <- noteInputFile "test/Test/golden/shelley/transaction-sign/initial-utxo2.skey"
+  utxoSigningKeyFile <- noteInputFile "test/Test/golden/shelley/transaction-sign/utxo.skey"
+  stakeSigningKeyFile <- noteInputFile "test/Test/golden/shelley/transaction-sign/stake.skey"
+  nodeColdSigningKeyFile <- noteInputFile "test/Test/golden/shelley/transaction-sign/node-cold.skey"
+  signedTransactionFile <- noteTempFile tempDir "signed.tx"
+  transactionPoolRegSignedFile <- noteTempFile tempDir "tx-pool-reg.signed"
+
+  -- Defaults to signing a Mainnet transaction
+
+  void $ execCardanoCLI
+    [ "shelley","transaction","sign"
+    , "--mainnet"
+    , "--tx-body-file", txBodyFile
+    , "--signing-key-file", initialUtxo1SigningKeyFile
+    , "--tx-file", signedTransactionFile
+    ]
+
+  assertFileOccurences 1 "TxSignedShelley" signedTransactionFile
+  assertEndsWithSingleNewline signedTransactionFile
+
+  -- Sign for a testnet with a testnet network magic of 11, but use two signing keys
+
+  void $ execCardanoCLI
+    [ "shelley","transaction","sign"
+    , "--mainnet"
+    , "--tx-body-file", txBodyFile
+    , "--signing-key-file", initialUtxo1SigningKeyFile
+    , "--signing-key-file", initialUtxo2SigningKeyFile
+    , "--tx-file", signedTransactionFile
+    ]
+
+  assertFileOccurences 1 "TxSignedShelley" signedTransactionFile
+  assertEndsWithSingleNewline signedTransactionFile
+
+  -- Sign a pool registration transaction.
+  -- TODO: This needs to use an unsigned tx with a registration certificate
+
+  void $ execCardanoCLI
+    [ "shelley","transaction","sign"
+    , "--mainnet"
+    , "--tx-body-file", txBodyFile
+    , "--signing-key-file", utxoSigningKeyFile
+    , "--signing-key-file", stakeSigningKeyFile
+    , "--signing-key-file", nodeColdSigningKeyFile
+    , "--tx-file", transactionPoolRegSignedFile
+    ]
+
+  assertFileOccurences 1 "TxSignedShelley" transactionPoolRegSignedFile
+  assertEndsWithSingleNewline transactionPoolRegSignedFile

IntersectMBO/cardano-cli

cardano-cli/test/Test/Golden/Shelley/StakeAddress/Build.hs
@@ -1,0 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Golden.Shelley.StakeAddress.Build
+  ( golden_shelleyStakeAddressBuild
+  ) where
+
+import           Cardano.Prelude
+import           Hedgehog (Property)
+import           Test.OptParse as OP
+
+{- HLINT ignore "Use camelCase" -}
+
+golden_shelleyStakeAddressBuild :: Property
+golden_shelleyStakeAddressBuild = propertyOnce . moduleWorkspace "tmp" $ \_ -> do
+  verificationKeyFile <- noteInputFile "test/data/golden/shelley/keys/stake_keys/verification_key"
+  goldenRewardAddressFile <- noteInputFile "test/data/golden/shelley/keys/stake_keys/reward_address"
+
+  rewardAddress <- execCardanoCLI
+    [ "shelley","stake-address","build"
+    , "--mainnet"
+    , "--staking-verification-key-file", verificationKeyFile
+    ]
+
+  goldenRewardsAddress <- OP.readFile goldenRewardAddressFile
+
+  equivalence rewardAddress goldenRewardsAddress

cardano-cli/test/Test/Golden/Shelley/StakeAddress/RegistrationCertificate.hs
@@ -1,0 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Golden.Shelley.StakeAddress.RegistrationCertificate
+  ( golden_shelleyStakeAddressRegistrationCertificate
+  ) where
+
+import           Cardano.Prelude
+import           Hedgehog (Property)
+import           Test.OptParse
+
+{- HLINT ignore "Use camelCase" -}
+
+golden_shelleyStakeAddressRegistrationCertificate :: Property
+golden_shelleyStakeAddressRegistrationCertificate = propertyOnce . moduleWorkspace "tmp" $ \tempDir -> do
+  keyGenStakingVerificationKeyFile <- noteInputFile "test/data/golden/shelley/keys/stake_keys/verification_key"
+  registrationCertFile <- noteTempFile tempDir "registration.cert"
+
+  void $ execCardanoCLI
+    [ "shelley","stake-address","registration-certificate"
+    , "--staking-verification-key-file", keyGenStakingVerificationKeyFile
+    , "--out-file", registrationCertFile
+    ]
+
+  assertFileOccurences 1 "Stake Address Registration Certificate" registrationCertFile
+
+  assertEndsWithSingleNewline registrationCertFile

cardano-cli/test/Test/Golden/Shelley/TextEnvelope/Keys/GenesisDelegateKeys.hs
@@ -1,0 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Golden.Shelley.TextEnvelope.Keys.GenesisDelegateKeys
+  ( golden_shelleyGenesisDelegateKeys
+  ) where
+
+import           Cardano.Api.Typed (AsType (..), HasTextEnvelope (..))
+import           Cardano.Prelude
+import           Hedgehog (Property)
+import           Test.OptParse
+
+-- | 1. Generate a key pair & operational certificate counter file
+--   2. Check for the existence of the key pair & counter file
+--   3. Check the TextEnvelope serialization format has not changed.
+golden_shelleyGenesisDelegateKeys :: Property
+golden_shelleyGenesisDelegateKeys = propertyOnce . moduleWorkspace "tmp" $ \tempDir -> do
+  -- Reference keys
+  referenceVerKey <- noteInputFile "test/data/golden/shelley/keys/genesis_delegate_keys/verification_key"
+  referenceSignKey <- noteInputFile "test/data/golden/shelley/keys/genesis_delegate_keys/signing_key"
+  referenceOpCertCounter <- noteInputFile "test/data/golden/shelley/keys/genesis_delegate_keys/operational_certificate_counter"
+
+  -- Key filepaths
+  verKey <- noteTempFile tempDir "genesis-delegate-verification-key-file"
+  signKey <- noteTempFile tempDir "genesis-delegate-signing-key-file"
+  opCertCounter <- noteTempFile tempDir "delegate-operational-cert-counter-file"
+
+  -- Generate payment verification key
+  void $ execCardanoCLI
+    [ "shelley","genesis","key-gen-delegate"
+    , "--verification-key-file", verKey
+    , "--signing-key-file", signKey
+    , "--operational-certificate-issue-counter-file", opCertCounter
+    ]
+
+  let signingKeyType = textEnvelopeType (AsSigningKey AsGenesisDelegateKey)
+      verificationKeyType = textEnvelopeType (AsVerificationKey AsGenesisDelegateKey)
+      operationalCertCounterType = textEnvelopeType AsOperationalCertificateIssueCounter
+
+  -- Check the newly created files have not deviated from the
+  -- golden files
+  checkTextEnvelopeFormat verificationKeyType referenceVerKey verKey
+  checkTextEnvelopeFormat signingKeyType referenceSignKey signKey
+  checkTextEnvelopeFormat operationalCertCounterType referenceOpCertCounter opCertCounter

cardano-cli/test/Test/Golden/Shelley/TextEnvelope/Keys/GenesisKeys.hs
@@ -1,0 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Golden.Shelley.TextEnvelope.Keys.GenesisKeys
+  ( golden_shelleyGenesisKeys
+  ) where
+
+import           Cardano.Api.Typed (AsType (..), HasTextEnvelope (..))
+import           Cardano.Prelude
+import           Hedgehog (Property)
+import           Test.OptParse
+
+-- | 1. Generate a key pair
+--   2. Check for the existence of the key pair
+--   3. Check the TextEnvelope serialization format has not changed
+golden_shelleyGenesisKeys :: Property
+golden_shelleyGenesisKeys = propertyOnce . moduleWorkspace "tmp" $ \tempDir -> do
+  -- Reference keys
+  referenceVerKey <- noteInputFile "test/data/golden/shelley/keys/genesis_keys/verification_key"
+  referenceSignKey <- noteInputFile "test/data/golden/shelley/keys/genesis_keys/signing_key"
+
+  -- Key filepaths
+  verKey <- noteTempFile tempDir "genesis-verification-key-file"
+  signKey <- noteTempFile tempDir "genesis-signing-key-file"
+
+  -- Generate payment verification key
+  void $ execCardanoCLI
+    [ "shelley","genesis","key-gen-genesis"
+    , "--verification-key-file", verKey
+    , "--signing-key-file", signKey
+    ]
+
+  let signingKeyType = textEnvelopeType (AsSigningKey AsGenesisKey)
+      verificationKeyType = textEnvelopeType (AsVerificationKey AsGenesisKey)
+
+  -- Check the newly created files have not deviated from the
+  -- golden files
+  checkTextEnvelopeFormat verificationKeyType referenceVerKey verKey
+  checkTextEnvelopeFormat signingKeyType referenceSignKey signKey

cardano-cli/test/Test/Golden/Shelley/TextEnvelope/Keys/VRFKeys.hs
@@ -1,0 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Golden.Shelley.TextEnvelope.Keys.VRFKeys
+  ( golden_shelleyVRFKeys
+  ) where
+
+import           Cardano.Api.Typed (AsType (..), HasTextEnvelope (..))
+import           Cardano.Prelude
+import           Hedgehog (Property)
+import           Test.OptParse
+
+-- | 1. Generate a key pair
+--   2. Check for the existence of the key pair
+--   3. Check the TextEnvelope serialization format has not changed.
+golden_shelleyVRFKeys :: Property
+golden_shelleyVRFKeys = propertyOnce . moduleWorkspace "tmp" $ \tempDir -> do
+  -- Reference keys
+  referenceVerKey <- noteInputFile "test/data/golden/shelley/keys/vrf_keys/verification_key"
+  referenceSignKey <- noteInputFile "test/data/golden/shelley/keys/vrf_keys/signing_key"
+
+  -- Key filepaths
+  verKey <- noteTempFile tempDir "vrf-verification-key-file"
+  signKey <- noteTempFile tempDir "vrf-signing-key-file"
+
+  -- Generate vrf verification key
+  void $ execCardanoCLI
+    [ "shelley","node","key-gen-VRF"
+    , "--verification-key-file", verKey
+    , "--signing-key-file", signKey
+    ]
+
+  let signingKeyType = textEnvelopeType (AsSigningKey AsVrfKey)
+      verificationKeyType = textEnvelopeType (AsVerificationKey AsVrfKey)
+
+  -- Check the newly created files have not deviated from the
+  -- golden files
+  checkTextEnvelopeFormat verificationKeyType referenceVerKey verKey
+  checkTextEnvelopeFormat signingKeyType referenceSignKey signKey

cardano-cli/test/Test/Golden/Shelley/TextView/DecodeCbor.hs
@@ -1,0 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Golden.Shelley.TextView.DecodeCbor
+  ( golden_shelleyTextViewDecodeCbor
+  ) where
+
+import           Cardano.Prelude
+import           Hedgehog (Property)
+import           Test.OptParse
+
+import qualified System.IO as IO
+
+{- HLINT ignore "Use camelCase" -}
+
+golden_shelleyTextViewDecodeCbor :: Property
+golden_shelleyTextViewDecodeCbor = propertyOnce $ moduleWorkspace "tmp" $ \tempDir -> do
+  unsignedTxFile <- noteInputFile "test/data/golden/shelley/tx/unsigned.tx"
+  decodedTxtFile <- noteTempFile tempDir "decoded.txt"
+
+  -- Defaults to signing a Mainnet transaction.
+
+  decodedTxt <- execCardanoCLI
+    [ "shelley","text-view","decode-cbor"
+    , "--file", unsignedTxFile
+    ]
+
+  liftIO $ IO.writeFile decodedTxtFile decodedTxt
+
+  assertFileOccurences 1 "# int(4999998000)" decodedTxtFile
+  assertFileOccurences 1 "# int(2000)" decodedTxtFile
+  assertFileOccurences 1 "# int(1000)" decodedTxtFile
+
+  assertEndsWithSingleNewline decodedTxtFile
+  assertFileLines (>= 10) decodedTxtFile

cardano-cli/test/Test/Golden/Shelley/Transaction/Sign.hs
@@ -1,0 +1,65 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Golden.Shelley.Transaction.Sign
+  ( golden_shelleyTransactionSign
+  ) where
+
+import           Cardano.Prelude
+import           Hedgehog (Property)
+import           Test.OptParse
+
+{- HLINT ignore "Use camelCase" -}
+
+golden_shelleyTransactionSign :: Property
+golden_shelleyTransactionSign = propertyOnce $ moduleWorkspace "tmp" $ \tempDir -> do
+  txBodyFile <- noteInputFile "test/data/golden/shelley/transaction-sign/tx-body-file"
+  initialUtxo1SigningKeyFile <- noteInputFile "test/data/golden/shelley/transaction-sign/initial-utxo1.skey"
+  initialUtxo2SigningKeyFile <- noteInputFile "test/data/golden/shelley/transaction-sign/initial-utxo2.skey"
+  utxoSigningKeyFile <- noteInputFile "test/data/golden/shelley/transaction-sign/utxo.skey"
+  stakeSigningKeyFile <- noteInputFile "test/data/golden/shelley/transaction-sign/stake.skey"
+  nodeColdSigningKeyFile <- noteInputFile "test/data/golden/shelley/transaction-sign/node-cold.skey"
+  signedTransactionFile <- noteTempFile tempDir "signed.tx"
+  transactionPoolRegSignedFile <- noteTempFile tempDir "tx-pool-reg.signed"
+
+  -- Defaults to signing a Mainnet transaction
+
+  void $ execCardanoCLI
+    [ "shelley","transaction","sign"
+    , "--mainnet"
+    , "--tx-body-file", txBodyFile
+    , "--signing-key-file", initialUtxo1SigningKeyFile
+    , "--tx-file", signedTransactionFile
+    ]
+
+  assertFileOccurences 1 "TxSignedShelley" signedTransactionFile
+  assertEndsWithSingleNewline signedTransactionFile
+
+  -- Sign for a testnet with a testnet network magic of 11, but use two signing keys
+
+  void $ execCardanoCLI
+    [ "shelley","transaction","sign"
+    , "--mainnet"
+    , "--tx-body-file", txBodyFile
+    , "--signing-key-file", initialUtxo1SigningKeyFile
+    , "--signing-key-file", initialUtxo2SigningKeyFile
+    , "--tx-file", signedTransactionFile
+    ]
+
+  assertFileOccurences 1 "TxSignedShelley" signedTransactionFile
+  assertEndsWithSingleNewline signedTransactionFile
+
+  -- Sign a pool registration transaction.
+  -- TODO: This needs to use an unsigned tx with a registration certificate
+
+  void $ execCardanoCLI
+    [ "shelley","transaction","sign"
+    , "--mainnet"
+    , "--tx-body-file", txBodyFile
+    , "--signing-key-file", utxoSigningKeyFile
+    , "--signing-key-file", stakeSigningKeyFile
+    , "--signing-key-file", nodeColdSigningKeyFile
+    , "--tx-file", transactionPoolRegSignedFile
+    ]
+
+  assertFileOccurences 1 "TxSignedShelley" transactionPoolRegSignedFile
+  assertEndsWithSingleNewline transactionPoolRegSignedFile