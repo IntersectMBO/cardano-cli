LintCroft/cardano-node

Expand upon and refactor Cardano.CLI.Shelley.Key

cardano-cli/src/Cardano/CLI/Shelley/Run/Governance.hs
@@ -14,7 +14,8 @@ import           Control.Monad.Trans.Except.Extra (firstExceptT, left, newExcept
 import           Cardano.Api.TextView (TextViewDescription (..), textShow)
 import           Cardano.Api.Typed
 
-import           Cardano.CLI.Shelley.Commands (VerificationKeyOrHashOrFile (..))
+import           Cardano.CLI.Shelley.Key (KeyDecodeError, VerificationKeyOrHashOrFile,
+                     readVerificationKeyOrHashOrFile, readVerificationKeyOrHashOrTextEnvFile)
 import           Cardano.CLI.Shelley.Parsers
 import           Cardano.CLI.Types
 
@@ -23,6 +24,7 @@ import qualified Shelley.Spec.Ledger.TxBody as Shelley
 
 data ShelleyGovernanceCmdError
   = ShelleyGovernanceCmdTextEnvReadError !(FileError TextEnvelopeError)
+  | ShelleyGovernanceCmdKeyReadError !(FileError KeyDecodeError)
   | ShelleyGovernanceCmdTextEnvWriteError !(FileError ())
   | ShelleyGovernanceCmdEmptyUpdateProposalError
   | ShelleyGovernanceCmdMIRCertificateKeyRewardMistmach
@@ -37,6 +39,7 @@ renderShelleyGovernanceError :: ShelleyGovernanceCmdError -> Text
 renderShelleyGovernanceError err =
   case err of
     ShelleyGovernanceCmdTextEnvReadError fileErr -> Text.pack (displayError fileErr)
+    ShelleyGovernanceCmdKeyReadError fileErr -> Text.pack (displayError fileErr)
     ShelleyGovernanceCmdTextEnvWriteError fileErr -> Text.pack (displayError fileErr)
     -- TODO: The equality check is still not working for empty update proposals.
     ShelleyGovernanceCmdEmptyUpdateProposalError ->
@@ -103,13 +106,13 @@ runGovernanceGenesisKeyDelegationCertificate genVkOrHashOrFp
                                              genDelVkOrHashOrFp
                                              vrfVkOrHashOrFp
                                              (OutputFile oFp) = do
-    genesisVkHash <- firstExceptT ShelleyGovernanceCmdTextEnvReadError
+    genesisVkHash <- firstExceptT ShelleyGovernanceCmdKeyReadError
       . newExceptT
-      $ readVerificationKeyOrHashOrFile AsGenesisKey genVkOrHashOrFp
-    genesisDelVkHash <-firstExceptT ShelleyGovernanceCmdTextEnvReadError
+      $ readVerificationKeyOrHashOrTextEnvFile AsGenesisKey genVkOrHashOrFp
+    genesisDelVkHash <-firstExceptT ShelleyGovernanceCmdKeyReadError
       . newExceptT
-      $ readVerificationKeyOrHashOrFile AsGenesisDelegateKey genDelVkOrHashOrFp
-    vrfVkHash <- firstExceptT ShelleyGovernanceCmdTextEnvReadError
+      $ readVerificationKeyOrHashOrTextEnvFile AsGenesisDelegateKey genDelVkOrHashOrFp
+    vrfVkHash <- firstExceptT ShelleyGovernanceCmdKeyReadError
       . newExceptT
       $ readVerificationKeyOrHashOrFile AsVrfKey vrfVkOrHashOrFp
     firstExceptT ShelleyGovernanceCmdTextEnvWriteError
@@ -139,21 +142,3 @@ runGovernanceUpdateProposal (OutputFile upFile) eNo genVerKeyFiles upPprams = do
         upProp = makeShelleyUpdateProposal upPprams genKeyHashes eNo
     firstExceptT ShelleyGovernanceCmdTextEnvWriteError . newExceptT $
       writeFileTextEnvelope upFile Nothing upProp
-
--- | Read a verification key or verification key hash or verification key file
--- and return a verification key hash.
---
--- If a filepath is provided, it will be interpreted as a text envelope
--- formatted file.
-readVerificationKeyOrHashOrFile
-  :: Key keyrole
-  => AsType keyrole
-  -> VerificationKeyOrHashOrFile keyrole
-  -> IO (Either (FileError TextEnvelopeError) (Hash keyrole))
-readVerificationKeyOrHashOrFile asType verKeyOrHashOrFile =
-  case verKeyOrHashOrFile of
-    VerificationKeyHash vkHash -> pure (Right vkHash)
-    VerificationKeyValue vk -> pure (Right $ verificationKeyHash vk)
-    VerificationKeyFilePath (VerificationKeyFile fp) -> do
-      eitherVk <- readFileTextEnvelope (AsVerificationKey asType) fp
-      pure (verificationKeyHash <$> eitherVk)

cardano-cli/src/Cardano/CLI/Shelley/Run/Node.hs
@@ -7,7 +7,7 @@ module Cardano.CLI.Shelley.Run.Node
 import           Cardano.Api.TextView (TextViewDescription (..))
 import           Cardano.Api.Typed
 import           Cardano.CLI.Shelley.Commands
-import           Cardano.CLI.Shelley.Key (SigningKeyDecodeError (..), readSigningKeyFileAnyOf)
+import           Cardano.CLI.Shelley.Key (KeyDecodeError (..), readSigningKeyFileAnyOf)
 import           Cardano.CLI.Types (SigningKeyFile (..), VerificationKeyFile (..))
 import           Cardano.Prelude
 import           Control.Monad.Trans.Except (ExceptT)
@@ -21,7 +21,7 @@ import qualified Data.Text as Text
 
 data ShelleyNodeCmdError
   = ShelleyNodeCmdReadFileError !(FileError TextEnvelopeError)
-  | ShelleyNodeCmdReadSigningKeyFileError !(FileError SigningKeyDecodeError)
+  | ShelleyNodeCmdReadKeyFileError !(FileError KeyDecodeError)
   | ShelleyNodeCmdWriteFileError !(FileError ())
   | ShelleyNodeCmdOperationalCertificateIssueError !OperationalCertIssueError
   deriving Show
@@ -31,7 +31,7 @@ renderShelleyNodeCmdError err =
   case err of
     ShelleyNodeCmdReadFileError fileErr -> Text.pack (displayError fileErr)
 
-    ShelleyNodeCmdReadSigningKeyFileError fileErr ->
+    ShelleyNodeCmdReadKeyFileError fileErr ->
       Text.pack (displayError fileErr)
 
     ShelleyNodeCmdWriteFileError fileErr -> Text.pack (displayError fileErr)
@@ -178,11 +178,11 @@ runNodeIssueOpCert (VerificationKeyFile vkeyKesPath)
       . newExceptT
       $ readFileTextEnvelope (AsVerificationKey AsKesKey) vkeyKesPath
 
-    signKey <- firstExceptT ShelleyNodeCmdReadSigningKeyFileError
+    signKey <- firstExceptT ShelleyNodeCmdReadKeyFileError
       . newExceptT
       $ readSigningKeyFileAnyOf
-          textEnvPossibleBlockIssuers
           bech32PossibleBlockIssuers
+          textEnvPossibleBlockIssuers
           stakePoolSKeyFile
 
     (ocert, nextOcertCtr) <-

IntersectMBO/cardano-cli

cardano-cli/src/Cardano/CLI/Shelley/Run/Governance.hs
@@ -14,7 +14,8 @@ import           Control.Monad.Trans.Except.Extra (firstExceptT, left, newExcept
 import           Cardano.Api.TextView (TextViewDescription (..), textShow)
 import           Cardano.Api.Typed
 
-import           Cardano.CLI.Shelley.Commands (VerificationKeyOrHashOrFile (..))
+import           Cardano.CLI.Shelley.Key (InputDecodeError, VerificationKeyOrHashOrFile,
+                     readVerificationKeyOrHashOrFile, readVerificationKeyOrHashOrTextEnvFile)
 import           Cardano.CLI.Shelley.Parsers
 import           Cardano.CLI.Types
 
@@ -23,6 +24,7 @@ import qualified Shelley.Spec.Ledger.TxBody as Shelley
 
 data ShelleyGovernanceCmdError
   = ShelleyGovernanceCmdTextEnvReadError !(FileError TextEnvelopeError)
+  | ShelleyGovernanceCmdKeyReadError !(FileError InputDecodeError)
   | ShelleyGovernanceCmdTextEnvWriteError !(FileError ())
   | ShelleyGovernanceCmdEmptyUpdateProposalError
   | ShelleyGovernanceCmdMIRCertificateKeyRewardMistmach
@@ -37,6 +39,7 @@ renderShelleyGovernanceError :: ShelleyGovernanceCmdError -> Text
 renderShelleyGovernanceError err =
   case err of
     ShelleyGovernanceCmdTextEnvReadError fileErr -> Text.pack (displayError fileErr)
+    ShelleyGovernanceCmdKeyReadError fileErr -> Text.pack (displayError fileErr)
     ShelleyGovernanceCmdTextEnvWriteError fileErr -> Text.pack (displayError fileErr)
     -- TODO: The equality check is still not working for empty update proposals.
     ShelleyGovernanceCmdEmptyUpdateProposalError ->
@@ -103,13 +106,13 @@ runGovernanceGenesisKeyDelegationCertificate genVkOrHashOrFp
                                              genDelVkOrHashOrFp
                                              vrfVkOrHashOrFp
                                              (OutputFile oFp) = do
-    genesisVkHash <- firstExceptT ShelleyGovernanceCmdTextEnvReadError
+    genesisVkHash <- firstExceptT ShelleyGovernanceCmdKeyReadError
       . newExceptT
-      $ readVerificationKeyOrHashOrFile AsGenesisKey genVkOrHashOrFp
-    genesisDelVkHash <-firstExceptT ShelleyGovernanceCmdTextEnvReadError
+      $ readVerificationKeyOrHashOrTextEnvFile AsGenesisKey genVkOrHashOrFp
+    genesisDelVkHash <-firstExceptT ShelleyGovernanceCmdKeyReadError
       . newExceptT
-      $ readVerificationKeyOrHashOrFile AsGenesisDelegateKey genDelVkOrHashOrFp
-    vrfVkHash <- firstExceptT ShelleyGovernanceCmdTextEnvReadError
+      $ readVerificationKeyOrHashOrTextEnvFile AsGenesisDelegateKey genDelVkOrHashOrFp
+    vrfVkHash <- firstExceptT ShelleyGovernanceCmdKeyReadError
       . newExceptT
       $ readVerificationKeyOrHashOrFile AsVrfKey vrfVkOrHashOrFp
     firstExceptT ShelleyGovernanceCmdTextEnvWriteError
@@ -139,21 +142,3 @@ runGovernanceUpdateProposal (OutputFile upFile) eNo genVerKeyFiles upPprams = do
         upProp = makeShelleyUpdateProposal upPprams genKeyHashes eNo
     firstExceptT ShelleyGovernanceCmdTextEnvWriteError . newExceptT $
       writeFileTextEnvelope upFile Nothing upProp
-
--- | Read a verification key or verification key hash or verification key file
--- and return a verification key hash.
---
--- If a filepath is provided, it will be interpreted as a text envelope
--- formatted file.
-readVerificationKeyOrHashOrFile
-  :: Key keyrole
-  => AsType keyrole
-  -> VerificationKeyOrHashOrFile keyrole
-  -> IO (Either (FileError TextEnvelopeError) (Hash keyrole))
-readVerificationKeyOrHashOrFile asType verKeyOrHashOrFile =
-  case verKeyOrHashOrFile of
-    VerificationKeyHash vkHash -> pure (Right vkHash)
-    VerificationKeyValue vk -> pure (Right $ verificationKeyHash vk)
-    VerificationKeyFilePath (VerificationKeyFile fp) -> do
-      eitherVk <- readFileTextEnvelope (AsVerificationKey asType) fp
-      pure (verificationKeyHash <$> eitherVk)

cardano-cli/src/Cardano/CLI/Shelley/Run/Node.hs
@@ -7,7 +7,7 @@ module Cardano.CLI.Shelley.Run.Node
 import           Cardano.Api.TextView (TextViewDescription (..))
 import           Cardano.Api.Typed
 import           Cardano.CLI.Shelley.Commands
-import           Cardano.CLI.Shelley.Key (SigningKeyDecodeError (..), readSigningKeyFileAnyOf)
+import           Cardano.CLI.Shelley.Key (InputDecodeError (..), readSigningKeyFileAnyOf)
 import           Cardano.CLI.Types (SigningKeyFile (..), VerificationKeyFile (..))
 import           Cardano.Prelude
 import           Control.Monad.Trans.Except (ExceptT)
@@ -21,7 +21,7 @@ import qualified Data.Text as Text
 
 data ShelleyNodeCmdError
   = ShelleyNodeCmdReadFileError !(FileError TextEnvelopeError)
-  | ShelleyNodeCmdReadSigningKeyFileError !(FileError SigningKeyDecodeError)
+  | ShelleyNodeCmdReadKeyFileError !(FileError InputDecodeError)
   | ShelleyNodeCmdWriteFileError !(FileError ())
   | ShelleyNodeCmdOperationalCertificateIssueError !OperationalCertIssueError
   deriving Show
@@ -31,7 +31,7 @@ renderShelleyNodeCmdError err =
   case err of
     ShelleyNodeCmdReadFileError fileErr -> Text.pack (displayError fileErr)
 
-    ShelleyNodeCmdReadSigningKeyFileError fileErr ->
+    ShelleyNodeCmdReadKeyFileError fileErr ->
       Text.pack (displayError fileErr)
 
     ShelleyNodeCmdWriteFileError fileErr -> Text.pack (displayError fileErr)
@@ -178,11 +178,11 @@ runNodeIssueOpCert (VerificationKeyFile vkeyKesPath)
       . newExceptT
       $ readFileTextEnvelope (AsVerificationKey AsKesKey) vkeyKesPath
 
-    signKey <- firstExceptT ShelleyNodeCmdReadSigningKeyFileError
+    signKey <- firstExceptT ShelleyNodeCmdReadKeyFileError
       . newExceptT
       $ readSigningKeyFileAnyOf
-          textEnvPossibleBlockIssuers
           bech32PossibleBlockIssuers
+          textEnvPossibleBlockIssuers
           stakePoolSKeyFile
 
     (ocert, nextOcertCtr) <-