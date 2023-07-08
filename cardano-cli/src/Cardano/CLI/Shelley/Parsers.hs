55c6e93b2113c53ff11962347896528d8a76cbcd
John Ky <john.ky@iohk.io>
2023-07-09 00:21:08 +1000

    Expose query constitution-hash command and tidy up parser

--- cardano-cli/src/Cardano/CLI/Shelley/Parsers.hs
+++ cardano-cli/src/Cardano/CLI/Shelley/Parsers.hs
@@ -15,8 +15,8 @@ module Cardano.CLI.Shelley.Parsers
   , parseTxIn
   ) where
 
-import           Cardano.Api
-import           Cardano.Api.Shelley
+import           Cardano.Api hiding (QueryInShelleyBasedEra(..))
+import           Cardano.Api.Shelley hiding (QueryInShelleyBasedEra(..))
 
 import           Cardano.Chain.Common (BlockCount (BlockCount))
 import           Cardano.CLI.Common.Parsers
@@ -907,43 +907,79 @@ pQueryCmd :: EnvCli -> Parser QueryCmd
 pQueryCmd envCli =
   asum
     [ subParser "protocol-parameters"
-        (Opt.info pQueryProtocolParameters $ Opt.progDesc "Get the node's current protocol parameters")
+        $ Opt.info pQueryProtocolParameters
+        $ Opt.progDesc "Get the node's current protocol parameters"
+    , subParser "constitution-hash"
+        $ Opt.info pQueryConstitutionHash
+        $ Opt.progDesc "Get the constitution hash"
     , subParser "tip"
-        (Opt.info pQueryTip $ Opt.progDesc "Get the node's current tip (slot no, hash, block no)")
+        $ Opt.info pQueryTip
+        $ Opt.progDesc "Get the node's current tip (slot no, hash, block no)"
     , subParser "stake-pools"
-        (Opt.info pQueryStakePools $ Opt.progDesc "Get the node's current set of stake pool ids")
+        $ Opt.info pQueryStakePools
+        $ Opt.progDesc "Get the node's current set of stake pool ids"
     , subParser "stake-distribution"
-        (Opt.info pQueryStakeDistribution $ Opt.progDesc "Get the node's current aggregated stake distribution")
+        $ Opt.info pQueryStakeDistribution
+        $ Opt.progDesc "Get the node's current aggregated stake distribution"
     , subParser "stake-address-info"
-        (Opt.info pQueryStakeAddressInfo $ Opt.progDesc "Get the current delegations and \
-                                                        \reward accounts filtered by stake \
-                                                        \address.")
+        $ Opt.info pQueryStakeAddressInfo
+        $ Opt.progDesc $ mconcat
+            [ "Get the current delegations and reward accounts filtered by stake address."
+            ]
     , subParser "utxo"
-        (Opt.info pQueryUTxO $ Opt.progDesc "Get a portion of the current UTxO: \
-                                            \by tx in, by address or the whole.")
+        $ Opt.info pQueryUTxO
+        $ Opt.progDesc $ mconcat
+            [ "Get a portion of the current UTxO: by tx in, by address or the whole."
+            ]
     , subParser "ledger-state"
-        (Opt.info pQueryLedgerState $ Opt.progDesc "Dump the current ledger state of the node (Ledger.NewEpochState -- advanced command)")
+        $ Opt.info pQueryLedgerState
+        $ Opt.progDesc $ mconcat
+            [ "Dump the current ledger state of the node (Ledger.NewEpochState -- advanced command)"
+            ]
     , subParser "protocol-state"
-        (Opt.info pQueryProtocolState $ Opt.progDesc "Dump the current protocol state of the node (Ledger.ChainDepState -- advanced command)")
+        $ Opt.info pQueryProtocolState
+        $ Opt.progDesc $ mconcat
+            [ "Dump the current protocol state of the node (Ledger.ChainDepState -- advanced command)"
+            ]
     , subParser "stake-snapshot"
-        (Opt.info pQueryStakeSnapshot $ Opt.progDesc "Obtain the three stake snapshots for a pool, plus the total active stake (advanced command)")
+        $ Opt.info pQueryStakeSnapshot
+        $ Opt.progDesc $ mconcat
+            [ "Obtain the three stake snapshots for a pool, plus the total active stake (advanced command)"
+            ]
     , hiddenSubParser "pool-params"
-        (Opt.info pQueryPoolState $ Opt.progDesc "DEPRECATED.  Use query pool-state instead.  Dump the pool parameters (Ledger.NewEpochState.esLState._delegationState._pState._pParams -- advanced command)")
+        $ Opt.info pQueryPoolState
+        $ Opt.progDesc $ mconcat
+            [ "DEPRECATED.  Use query pool-state instead.  Dump the pool parameters "
+            , "(Ledger.NewEpochState.esLState._delegationState._pState._pParams -- advanced command)"
+            ]
     , subParser "leadership-schedule"
-        (Opt.info pLeadershipSchedule $ Opt.progDesc "Get the slots the node is expected to mint a block in (advanced command)")
+        $ Opt.info pLeadershipSchedule
+        $ Opt.progDesc "Get the slots the node is expected to mint a block in (advanced command)"
     , subParser "kes-period-info"
-        (Opt.info pKesPeriodInfo $ Opt.progDesc "Get information about the current KES period and your node's operational certificate.")
+        $ Opt.info pKesPeriodInfo
+        $ Opt.progDesc "Get information about the current KES period and your node's operational certificate."
     , subParser "pool-state"
-        (Opt.info pQueryPoolState $ Opt.progDesc "Dump the pool state")
+        $ Opt.info pQueryPoolState
+        $ Opt.progDesc "Dump the pool state"
     , subParser "tx-mempool"
-        (Opt.info pQueryTxMempool $ Opt.progDesc "Local Mempool info")
+        $ Opt.info pQueryTxMempool
+        $ Opt.progDesc "Local Mempool info"
     , subParser "slot-number"
-        (Opt.info pQuerySlotNumber $ Opt.progDesc "Query slot number for UTC timestamp")
+        $ Opt.info pQuerySlotNumber
+        $ Opt.progDesc "Query slot number for UTC timestamp"
     ]
   where
     pQueryProtocolParameters :: Parser QueryCmd
     pQueryProtocolParameters =
       QueryProtocolParameters'
+        <$> pSocketPath envCli
+        <*> pConsensusModeParams
+        <*> pNetworkId envCli
+        <*> pMaybeOutputFile
+
+    pQueryConstitutionHash :: Parser QueryCmd
+    pQueryConstitutionHash =
+      QueryConstitutionHash
         <$> pSocketPath envCli
         <*> pConsensusModeParams
         <*> pNetworkId envCli
