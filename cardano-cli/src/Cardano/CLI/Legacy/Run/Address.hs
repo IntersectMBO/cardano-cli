baf8778415cdd6f6bcff63efae49adfffc8bcf86
John Ky <john.ky@iohk.io>
2023-08-20 23:26:26 +1000

    Move ShelleyAddressInfoError to own module

--- cardano-cli/src/Cardano/CLI/Legacy/Run/Address.hs
+++ cardano-cli/src/Cardano/CLI/Legacy/Run/Address.hs
@@ -18,8 +18,9 @@ import           Cardano.Api
 import           Cardano.Api.Shelley
 
 import           Cardano.CLI.Legacy.Commands.Address
-import           Cardano.CLI.Legacy.Run.Address.Info (ShelleyAddressInfoError, runAddressInfo)
+import           Cardano.CLI.Legacy.Run.Address.Info (runAddressInfo)
 import           Cardano.CLI.Read
+import           Cardano.CLI.Types.Errors.ShelleyAddressInfoError
 import           Cardano.CLI.Types.Key (PaymentVerifier (..), StakeIdentifier (..),
                    StakeVerifier (..), VerificationKeyTextOrFile,
                    VerificationKeyTextOrFileError (..), generateKeyPair, readVerificationKeyOrFile,
