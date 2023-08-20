6e657d6cb45c8c477e5d992aee0dc613238c6b41
John Ky <john.ky@iohk.io>
2023-08-21 00:20:09 +1000

    Move to CardanoAddressSigningKeyConversionError to own module

--- cardano-cli/src/Cardano/CLI/Legacy/Run/Key.hs
+++ cardano-cli/src/Cardano/CLI/Legacy/Run/Key.hs
@@ -22,6 +22,7 @@ import           Cardano.Api.Shelley
 import qualified Cardano.CLI.Byron.Key as Byron
 import           Cardano.CLI.Legacy.Commands.Key
 import           Cardano.CLI.Types.Common
+import           Cardano.CLI.Types.Errors.CardanoAddressSigningKeyConversionError
 import           Cardano.CLI.Types.Errors.ItnKeyConversionError
 import           Cardano.CLI.Types.Key
 import qualified Cardano.Crypto.DSIGN as DSIGN
@@ -43,7 +44,6 @@ import           Data.Text (Text)
 import qualified Data.Text as Text
 import qualified Data.Text.Encoding as Text
 import           System.Exit (exitFailure)
-
 
 data ShelleyKeyCmdError
   = ShelleyKeyCmdReadFileError !(FileError TextEnvelopeError)
@@ -515,32 +515,6 @@ data SomeCardanoAddressSigningKey
   = ACardanoAddrShelleyPaymentSigningKey !(SigningKey PaymentExtendedKey)
   | ACardanoAddrShelleyStakeSigningKey !(SigningKey StakeExtendedKey)
   | ACardanoAddrByronSigningKey !(SigningKey ByronKey)
-
--- | An error that can occur while converting a @cardano-address@ extended
--- signing key.
-data CardanoAddressSigningKeyConversionError
-  = CardanoAddressSigningKeyBech32DecodeError !Bech32DecodeError
-  -- ^ There was an error in decoding the string as Bech32.
-  | CardanoAddressSigningKeyDeserialisationError !ByteString
-  -- ^ There was an error in converting the @cardano-address@ extended signing
-  -- key.
-  deriving (Show, Eq)
-
-instance Error CardanoAddressSigningKeyConversionError where
-  displayError = Text.unpack . renderCardanoAddressSigningKeyConversionError
-
--- | Render an error message for a 'CardanoAddressSigningKeyConversionError'.
-renderCardanoAddressSigningKeyConversionError
-  :: CardanoAddressSigningKeyConversionError
-  -> Text
-renderCardanoAddressSigningKeyConversionError err =
-  case err of
-    CardanoAddressSigningKeyBech32DecodeError decErr ->
-      Text.pack (displayError decErr)
-    CardanoAddressSigningKeyDeserialisationError _bs ->
-      -- Sensitive data, such as the signing key, is purposely not included in
-      -- the error message.
-      "Error deserialising cardano-address signing key."
 
 -- | Decode a Bech32-encoded string.
 decodeBech32
