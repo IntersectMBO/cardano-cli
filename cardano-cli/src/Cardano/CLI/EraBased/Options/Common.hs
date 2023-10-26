86566f8582b2289b81cfb2125daac77ed001e664
John Ky <john.ky@iohk.io>
2023-10-26 18:02:35 +1100

    Remove --shelley-mode and --byron-mode

--- cardano-cli/src/Cardano/CLI/EraBased/Options/Common.hs
+++ cardano-cli/src/Cardano/CLI/EraBased/Options/Common.hs
@@ -111,26 +111,10 @@ toUnitIntervalOrErr r = case Ledger.boundRational r of
 
 pConsensusModeParams :: Parser AnyConsensusModeParams
 pConsensusModeParams = asum
-  [ pShelleyMode *> pShelleyConsensusMode
-  , pByronMode *> pByronConsensusMode
-  , pCardanoMode *> pCardanoConsensusMode
+  [ pCardanoMode *> pCardanoConsensusMode
   , pDefaultConsensusMode
   ]
   where
-    pShelleyMode :: Parser ()
-    pShelleyMode =
-      Opt.flag' () $ mconcat
-        [ Opt.long "shelley-mode"
-        , Opt.help "For talking to a node running in Shelley-only mode."
-        ]
-
-    pByronMode :: Parser ()
-    pByronMode =
-      Opt.flag' () $ mconcat
-        [ Opt.long "byron-mode"
-        , Opt.help "For talking to a node running in Byron-only mode."
-        ]
-
     pCardanoMode :: Parser ()
     pCardanoMode =
       Opt.flag' () $ mconcat
@@ -140,12 +124,6 @@ pConsensusModeParams = asum
 
     pCardanoConsensusMode :: Parser AnyConsensusModeParams
     pCardanoConsensusMode = AnyConsensusModeParams . CardanoModeParams <$> pEpochSlots
-
-    pByronConsensusMode :: Parser AnyConsensusModeParams
-    pByronConsensusMode = AnyConsensusModeParams . ByronModeParams <$> pEpochSlots
-
-    pShelleyConsensusMode :: Parser AnyConsensusModeParams
-    pShelleyConsensusMode = pure (AnyConsensusModeParams ShelleyModeParams)
 
     pDefaultConsensusMode :: Parser AnyConsensusModeParams
     pDefaultConsensusMode =
