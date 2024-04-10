645caaca000e0fefa99da67b6cfed918e467e180
Clément Hurlin <clement.hurlin@moduscreate.com>
2024-04-10 12:00:11 +0200

    Detail in tests: remove useless checks (CLI call would fail anyway)

--- cardano-cli/test/cardano-cli-golden/Test/Golden/Key/NonExtendedKey.hs
+++ cardano-cli/test/cardano-cli-golden/Test/Golden/Key/NonExtendedKey.hs
@@ -24,8 +24,6 @@ hprop_golden_KeyNonExtendedKey_GenesisExtendedVerificationKey =
     nonExtendedFp <-  H.note "test/cardano-cli-golden/files/golden/key/non-extended-keys/non-extended-shelley.000.vkey"
     outFp <- H.note $ tempDir </> "non-extended-shelley.000.vkey"
 
-    H.assertFilesExist [genesisVKeyFp]
-
     -- Convert the `cardano-address` signing key
     void $ execCardanoCLI
       [ "key", "non-extended-key"
@@ -43,8 +41,6 @@ hprop_golden_KeyNonExtendedKey_StakeExtendedVerificationKeyShelley =
     genesisVKeyFp <- H.noteInputFile "test/cardano-cli-golden/files/input/key/non-extended-keys/stake.000.vkey"
     nonExtendedFp <-  H.note "test/cardano-cli-golden/files/golden/key/non-extended-keys/non-extended-stake.000.vkey"
     outFp <- H.note $ tempDir </> "non-extended-stake.000.vkey"
-
-    H.assertFilesExist [genesisVKeyFp]
 
     -- Convert the `cardano-address` signing key
     void $ execCardanoCLI
@@ -64,8 +60,6 @@ hprop_golden_KeyNonExtendedKey_DRepExtendedVerificationKey =
     goldenFile <-  H.note "test/cardano-cli-golden/files/golden/key/non-extended-keys/non-extended-drep.vkey"
     outFp <- H.note $ tempDir </> "non-extended-drep.vkey"
 
-    H.assertFilesExist [extendedKeyFile]
-
     void $ execCardanoCLI
       [ "conway", "key", "non-extended-key"
       , "--extended-verification-key-file", extendedKeyFile
@@ -82,8 +76,6 @@ hprop_golden_extended_payment_vkey_to_non_extended_vkey =
     extendedKeyFile <- H.noteInputFile "test/cardano-cli-golden/files/input/key/non-extended-keys/extended-payment.vkey"
     goldenFile <-  H.note "test/cardano-cli-golden/files/golden/key/non-extended-keys/non-extended-payment.vkey"
     outFp <- H.note $ tempDir </> "non-extended-payment.vkey"
-
-    H.assertFilesExist [extendedKeyFile]
 
     void $ execCardanoCLI
       [ "conway", "key", "non-extended-key"
