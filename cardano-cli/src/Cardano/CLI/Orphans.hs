996e08185b7ec11406e79912ea638295526b587b
Pablo Lamela <pablo.lamela@iohk.io>
2024-10-10 23:36:59 +0200

    Remove transitive dependencies to `cardano-ledger-shelley`

--- cardano-cli/src/Cardano/CLI/Orphans.hs
+++ cardano-cli/src/Cardano/CLI/Orphans.hs
@@ -6,9 +6,9 @@ module Cardano.CLI.Orphans
 where
 
 import           Cardano.Api
+import qualified Cardano.Api.Ledger as L
 
 import qualified Cardano.Ledger.Api as L
-import qualified Cardano.Ledger.Shelley.LedgerState as L
 
 import           Data.Aeson
 
