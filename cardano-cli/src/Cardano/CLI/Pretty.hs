41ca93425f12939cf5fb7248387ebbd50f82bdd5
Jordan Millar <jordan.millar@iohk.io>
2024-10-22 10:54:36 -0400

    Remove unused functions from Cardano.CLI.Pretty

--- cardano-cli/src/Cardano/CLI/Pretty.hs
+++ cardano-cli/src/Cardano/CLI/Pretty.hs
@@ -1,8 +1,6 @@
 module Cardano.CLI.Pretty
-  ( putLn
-  , hPutLn
-  -- Re-exported functions from Cardano.Api related to pretty-printing
-  , black
+  ( -- Re-exported functions from Cardano.Api related to pretty-printing
+    black
   , blue
   , cyan
   , docToLazyText
@@ -29,23 +27,3 @@ where
 import           Cardano.Api (Ann, Doc, MonadIO (..), Pretty (..), ShowOf (..), black, blue, cyan,
                    docToLazyText, docToString, docToText, green, hsep, magenta, prettyException,
                    pshow, red, vsep, white, yellow, (<+>))
-
-import qualified Control.Concurrent.QSem as IO
-import           Control.Exception (bracket_)
-import qualified Data.Text.Lazy.IO as TextLazy
-import           Prettyprinter.Render.Terminal
-import qualified System.IO as IO
-import qualified System.IO.Unsafe as IO
-
-sem :: IO.QSem
-sem = IO.unsafePerformIO $ IO.newQSem 1
-{-# NOINLINE sem #-}
-
-consoleBracket :: IO a -> IO a
-consoleBracket = bracket_ (IO.waitQSem sem) (IO.signalQSem sem)
-
-putLn :: MonadIO m => Doc AnsiStyle -> m ()
-putLn = liftIO . consoleBracket . TextLazy.putStrLn . docToLazyText
-
-hPutLn :: MonadIO m => IO.Handle -> Doc AnsiStyle -> m ()
-hPutLn h = liftIO . consoleBracket . TextLazy.hPutStr h . docToLazyText
