b6ffd14d24f4471d8f7397ace4b08a3c521f28fd
Pablo Lamela <pablo.lamela@iohk.io>
2025-03-06 16:29:53 +0100

    Add offline test for `calculate-plutus-script-cost` command

--- cardano-cli/test/cardano-cli-test/Test/Cli/Transaction/Build.hs
+++ cardano-cli/test/cardano-cli-test/Test/Cli/Transaction/Build.hs
@@ -1,14 +1,47 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Test.Cli.Transaction.Build where
 
+import Cardano.Api.Ledger (hashToBytes)
+import Cardano.Api.Shelley
+  ( AddressAny (AddressShelley)
+  , AddressInEra (AddressInEra)
+  , AddressTypeInEra (ShelleyAddressInEra)
+  , AsType (AsAddressAny)
+  , ReferenceScript (ReferenceScriptNone)
+  , ShelleyBasedEra (ShelleyBasedEraConway)
+  , TxId (TxId)
+  , TxIn (TxIn)
+  , TxIx (TxIx)
+  , TxOut (TxOut)
+  , TxOutDatum (TxOutDatumNone)
+  , UTxO (UTxO)
+  , deserialiseAddress
+  , liftIO
+  , lovelaceToTxOutValue
+  , writeFileJSON
+  )
+
+import Data.Aeson qualified as Aeson
+import Data.ByteString.Base16 qualified as Base16
 import Data.List (isInfixOf)
+import Data.Map qualified as Map
+import Data.Text qualified as Text
+import Data.Text.Encoding qualified as Text
+import Lens.Micro ((^?))
+import Lens.Micro.Aeson qualified as Aeson
 import System.Exit (ExitCode (..))
 import System.FilePath ((</>))
+
+import Test.Gen.Cardano.Api.Typed (genShelleyHash)
 
 import Test.Cardano.CLI.Util
 
 import Hedgehog
 import Hedgehog qualified as H
-import Hedgehog.Extras.Test.Base qualified as H
+import Hedgehog.Extras qualified as H
+import Hedgehog.Gen qualified as Gen
+import Hedgehog.Range qualified as Range
 
 inputDir :: FilePath
 inputDir = "test/cardano-cli-test/files/input/shelley/transaction"
@@ -93,3 +126,86 @@ hprop_conway_transaction_build_raw_negative_bits_positive_total_txout = watchdog
       , "--out-file"
       , outFile
       ]
+
+-- | This tests calculating the cost of a plutus script offline.
+-- @cabal test cardano-cli-test --test-options '-p "/conway calculate plutus script cost offline/"'@
+hprop_conway_calculate_plutus_script_cost_offline :: Property
+hprop_conway_calculate_plutus_script_cost_offline = propertyOnce $ H.moduleWorkspace "tmp" $ \tempDir -> do
+  randomTxIdHash <- forAll genShelleyHash
+  txIx <- forAll $ Gen.integral (Range.linear 0 10)
+  AddressShelley scriptAddr <-
+    H.evalMaybe $
+      deserialiseAddress AsAddressAny "addr_test1wqvxuvh64q9zdqgrjt76d42eclk5wgdxtnsun4808cwg0dqxy2mj0"
+
+  unsignedTx <- H.noteTempFile tempDir "unsigned.tx"
+
+  H.noteShowM_ $
+    execCardanoCLI
+      [ "conway"
+      , "transaction"
+      , "build-raw"
+      , "--tx-in"
+      , Text.unpack (Text.decodeLatin1 (Base16.encode (hashToBytes randomTxIdHash))) <> "#" <> show txIx
+      , "--tx-in-script-file"
+      , "test/cardano-cli-test/files/input/plutus/v3-always-succeeds.plutus"
+      , "--tx-in-redeemer-value"
+      , "42"
+      , "--tx-in-execution-units"
+      , "(30,40)"
+      , "--tx-out"
+      , "addr_test1wqvxuvh64q9zdqgrjt76d42eclk5wgdxtnsun4808cwg0dqxy2mj0+1000000"
+      , "--fee"
+      , "200000"
+      , "--out-file"
+      , unsignedTx
+      ]
+
+  costOutFile <- H.noteTempFile tempDir "cost-calculation.json"
+
+  utxoFile <- H.noteTempFile tempDir "utxo.json"
+  H.evalEitherM $
+    liftIO $
+      writeFileJSON
+        utxoFile
+        ( UTxO $
+            Map.singleton
+              (TxIn (TxId randomTxIdHash) (TxIx txIx))
+              ( TxOut
+                  (AddressInEra (ShelleyAddressInEra ShelleyBasedEraConway) scriptAddr)
+                  (lovelaceToTxOutValue ShelleyBasedEraConway 1200000)
+                  TxOutDatumNone
+                  ReferenceScriptNone
+              )
+        )
+
+  H.noteShowM_ $
+    execCardanoCLI
+      [ "conway"
+      , "transaction"
+      , "calculate-plutus-script-cost"
+      , "--start-time-posix"
+      , "1666656000"
+      , "--protocol-params-file"
+      , "test/cardano-cli-test/files/input/protocol-params-preview.json"
+      , "--utxo-file"
+      , utxoFile
+      , "--unsafe-extend-safe-zone"
+      , "--era-history-file"
+      , "test/cardano-cli-test/files/input/preview-era-history.json"
+      , "--tx-file"
+      , unsignedTx
+      , "--out-file"
+      , costOutFile
+      ]
+
+  json :: Aeson.Value <- H.evalEitherM $ H.readJsonFile costOutFile
+
+  lovelaceCost <- H.evalMaybe $ json ^? Aeson.nth 0 . Aeson.key "lovelaceCost" . Aeson._Number
+  lovelaceCost === 34
+
+  executionUnits <- H.evalMaybe $ json ^? Aeson.nth 0 . Aeson.key "executionUnits"
+  memory <- H.evalMaybe $ executionUnits ^? Aeson.key "memory" . Aeson._Number
+  memory === 500
+
+  steps <- H.evalMaybe $ executionUnits ^? Aeson.key "steps" . Aeson._Number
+  steps === 64100
