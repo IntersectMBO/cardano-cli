aae4b52b2b0c7090df5a06e57d5de445a7002e97
Pablo Lamela <pablo.lamela@iohk.io>
2025-04-02 12:17:05 +0200

    Changes while writing the tutorial

--- cardano-cli/src/Cardano/CLI/EraIndependent/Debug/TryIpc/Run.hs
+++ cardano-cli/src/Cardano/CLI/EraIndependent/Debug/TryIpc/Run.hs
@@ -7,17 +7,16 @@ module Cardano.CLI.EraIndependent.Debug.TryIpc.Run
   )
 where
 
-import Cardano.Api
-import Cardano.Api.Consensus (EraMismatch (..))
-import Cardano.Api.Network (Target (VolatileTip))
-import Cardano.Api.Shelley
-  ( AcquiringFailure (AFPointNotOnChain, AFPointTooOld)
-  , ReferenceScript (ReferenceScriptNone)
-  )
+import Cardano.Api qualified as Api
+import Cardano.Api.Consensus qualified as Consensus
+import Cardano.Api.Network qualified as Network
+import Cardano.Api.Shelley qualified as Shelley
 
-import Cardano.Prelude ((&))
+import Prelude
 
 import Control.Monad (when)
+import Control.Monad.Except (runExceptT)
+import Data.Function ((&))
 import Data.Map qualified as Map
 import Data.Set qualified as Set
 
@@ -25,31 +24,31 @@ runTryIpcCmd :: IO ()
 runTryIpcCmd = do
   -- We set the connection parameters
   let connectionInfo =
-        LocalNodeConnectInfo
-          { localConsensusModeParams = CardanoModeParams (EpochSlots 86_400) -- from shelley-genesis.json "epochLength" (for mainnet 432_000)
-          , localNodeNetworkId = Testnet (NetworkMagic 2) -- from shelley-genesis.json "netorkMagic"
-          , localNodeSocketPath = File "/Users/palas/preview/state-node-preview/node.socket" -- socket file provided by the node
+        Api.LocalNodeConnectInfo
+          { Api.localConsensusModeParams = Api.CardanoModeParams (Api.EpochSlots 86_400) -- from shelley-genesis.json "epochLength" (for mainnet 432_000)
+          , Api.localNodeNetworkId = Api.Testnet (Api.NetworkMagic 2) -- from shelley-genesis.json "netorkMagic"
+          , Api.localNodeSocketPath = Api.File "/Users/palas/preview/state-node-preview/node.socket" -- socket file provided by the node
           }
   -- We make a query to obtain the current era
-  eEra <- runExceptT $ queryNodeLocalState connectionInfo VolatileTip QueryCurrentEra
+  eEra <- runExceptT $ Api.queryNodeLocalState connectionInfo Network.VolatileTip Api.QueryCurrentEra
 
   -- We unwrap the error and make sure it is not Byron
-  AnyShelleyBasedEra sbe :: AnyShelleyBasedEra <- case eEra of
-    Right (AnyCardanoEra era) ->
-      caseByronOrShelleyBasedEra
+  Api.AnyShelleyBasedEra sbe :: Api.AnyShelleyBasedEra <- case eEra of
+    Right (Api.AnyCardanoEra era) ->
+      Api.caseByronOrShelleyBasedEra
         (error "We are in Byron era")
-        (return . AnyShelleyBasedEra)
+        (return . Api.AnyShelleyBasedEra)
         era
-    Left AFPointTooOld -> error "Error, point queried in the chain is too old!"
-    Left AFPointNotOnChain -> error "Error, point queried is not on chain!"
+    Left Shelley.AFPointTooOld -> error "Error, point queried in the chain is too old!"
+    Left Shelley.AFPointNotOnChain -> error "Error, point queried is not on chain!"
 
-  -- We make a query to obtain the number of UTxOs at the moment
+  -- -- We make a query to obtain the number of UTxOs at the moment
   -- eUtxo <-
   --   runExceptT $
-  --     queryNodeLocalState
+  --     Api.queryNodeLocalState
   --       connectionInfo
-  --       VolatileTip
-  --       (QueryInEra (QueryInShelleyBasedEra sbe (QueryUTxO QueryUTxOWhole)))
+  --       Network.VolatileTip
+  --       (Api.QueryInEra (Api.QueryInShelleyBasedEra sbe (Api.QueryUTxO Api.QueryUTxOWhole)))
 
   -- -- We unwrap the error and print the number of UTxOs
   -- case eUtxo of
@@ -60,14 +59,14 @@ runTryIpcCmd = do
   --   Left AFPointTooOld -> error "Error, point queried in the chain is too old!"
   --   Left AFPointNotOnChain -> error "Error, point queried is not on chain!"
 
-  let address = case deserialiseFromBech32
-        (AsAddress AsShelleyAddr)
+  let address = case Api.deserialiseFromBech32
+        (Api.AsAddress Api.AsShelleyAddr)
         "addr_test1qqc536zmzggkvkes4lf2jpjxw5g0488f656yqlwpuw0uu3fk9tjxma0kpssjkg2e9ltgrl0tvfqay0lr7k4qzdyqya3s6udgu7" of
         Right addr -> addr
         Left err -> error $ "Error deserialising address: " ++ show err
 
-  let destAddress = case deserialiseFromBech32
-        (AsAddress AsShelleyAddr)
+  let destAddress = case Api.deserialiseFromBech32
+        (Api.AsAddress Api.AsShelleyAddr)
         "addr_test1vqeux7xwusdju9dvsj8h7mca9aup2k439kfmwy773xxc2hcu7zy99" of
         Right addr -> addr
         Left err -> error $ "Error deserialising address: " ++ show err
@@ -75,28 +74,36 @@ runTryIpcCmd = do
   -- We make a query to obtain the UTxOs for the given address
   eUtxo <-
     runExceptT $
-      queryNodeLocalState
+      Api.queryNodeLocalState
         connectionInfo
-        VolatileTip
-        ( QueryInEra
-            (QueryInShelleyBasedEra sbe (QueryUTxO (QueryUTxOByAddress $ Set.singleton $ AddressShelley address)))
+        Network.VolatileTip
+        ( Api.QueryInEra
+            ( Api.QueryInShelleyBasedEra
+                sbe
+                (Api.QueryUTxO (Api.QueryUTxOByAddress $ Set.singleton $ Api.AddressShelley address))
+            )
         )
 
   -- We unwrap the error and print the number of UTxOs
   utxo <- case eUtxo of
-    Right (Right (UTxO utxo)) -> do
+    Right (Right (Api.UTxO utxo)) -> do
       return utxo
-    Right (Left (EraMismatch{ledgerEraName, otherEraName})) ->
-      error ("Error we assumed era was " ++ show otherEraName ++ " but it was " ++ show ledgerEraName)
-    Left AFPointTooOld -> error "Error, point queried in the chain is too old!"
-    Left AFPointNotOnChain -> error "Error, point queried is not on chain!"
+    Right (Left (Consensus.EraMismatch{Consensus.ledgerEraName, Consensus.otherEraName})) ->
+      error
+        ( "Error, we assumed era was "
+            ++ show otherEraName
+            ++ " but it was "
+            ++ show ledgerEraName
+        )
+    Left Shelley.AFPointTooOld -> error "Error, point queried in the chain is too old!"
+    Left Shelley.AFPointNotOnChain -> error "Error, point queried is not on chain!"
 
   when (Map.null utxo) $
     error "Error, no UTxOs found for the given address!"
 
   let txIns =
         [ ( txIn
-          , BuildTxWith (KeyWitness KeyWitnessForSpending)
+          , Api.BuildTxWith (Api.KeyWitness Api.KeyWitnessForSpending)
           )
         | (txIn, _) <- Map.toList utxo
         ]
@@ -105,51 +112,51 @@ runTryIpcCmd = do
 
   let totalValue =
         sum
-          [ txOutValueToLovelace txOutValue
-          | (_, TxOut _ txOutValue _ _) <- Map.toList utxo
+          [ Api.txOutValueToLovelace txOutValue
+          | (_, Api.TxOut _ txOutValue _ _) <- Map.toList utxo
           ]
           - feeAmount
 
   let txOut =
-        TxOut
-          ( AddressInEra
-              (ShelleyAddressInEra sbe)
+        Api.TxOut
+          ( Api.AddressInEra
+              (Api.ShelleyAddressInEra sbe)
               destAddress
           )
-          (lovelaceToTxOutValue sbe totalValue)
-          TxOutDatumNone
-          ReferenceScriptNone
+          (Api.lovelaceToTxOutValue sbe totalValue)
+          Api.TxOutDatumNone
+          Shelley.ReferenceScriptNone
 
-  let txFee = TxFeeExplicit sbe feeAmount
+  let txFee = Api.TxFeeExplicit sbe feeAmount
 
   let txBodyContent =
-        defaultTxBodyContent sbe
-          & setTxIns txIns
-          & setTxOuts [txOut]
-          & setTxFee txFee
+        Api.defaultTxBodyContent sbe
+          & Api.setTxIns txIns
+          & Api.setTxOuts [txOut]
+          & Api.setTxFee txFee
   eSigningKey <-
-    readFileTextEnvelope
-      (AsSigningKey AsPaymentKey)
+    Api.readFileTextEnvelope
+      (Api.AsSigningKey Api.AsPaymentKey)
       "/Users/palas/work/cardano-cli/test/address1/key.skey"
 
   let signingKey = case eSigningKey of
         Right sk -> sk
         Left err -> error $ "Error deserialising signing key: " ++ show err
 
-  let witness = WitnessPaymentKey signingKey
+  let witness = Api.WitnessPaymentKey signingKey
 
-  let eTxBody = createTransactionBody sbe txBodyContent
+  let eTxBody = Api.createTransactionBody sbe txBodyContent
 
   let txBody = case eTxBody of
         Right txBody' -> txBody'
         Left err -> error $ "Error creating transaction body: " ++ show err
 
-  let signedTx = signShelleyTransaction sbe txBody [witness]
+  let signedTx = Api.signShelleyTransaction sbe txBody [witness]
 
-  result <- submitTxToNodeLocal connectionInfo (TxInMode sbe signedTx)
+  result <- Api.submitTxToNodeLocal connectionInfo (Api.TxInMode sbe signedTx)
 
   case result of
-    SubmitSuccess -> putStrLn "Transaction submitted successfully!"
-    SubmitFail reason -> error $ "Error submitting transaction: " ++ show reason
+    Api.SubmitSuccess -> putStrLn "Transaction submitted successfully!"
+    Api.SubmitFail reason -> error $ "Error submitting transaction: " ++ show reason
 
   return ()
