5080597f659943314164245779af0ecaa12c628c
Pablo Lamela <pablo.lamela@iohk.io>
2025-04-14 19:47:59 +0200

    Make description of error `TxCmdUTxOJSONError` more precise

--- cardano-cli/src/Cardano/CLI/Type/Error/TxCmdError.hs
+++ cardano-cli/src/Cardano/CLI/Type/Error/TxCmdError.hs
@@ -254,7 +254,7 @@ renderTxCmdError = \case
   TxCmdUTxOFileError e ->
     "Error while reading UTxO set from JSON file: " <> prettyError e
   TxCmdUTxOJSONError e ->
-    "Error while reading UTxO set from JSON file: " <> pretty e
+    "Error while decoding JSON from UTxO set file: " <> pretty e
   TxCmdGenesisDataError genesisDataError ->
     "Error while reading Byron genesis data: " <> pshow (toLazyText $ build genesisDataError)
 
