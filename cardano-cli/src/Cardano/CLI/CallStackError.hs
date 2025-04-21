fd431b4b4891df9b4a6daed914a23e55da7ef02a
Pablo Lamela <pablo.lamela@iohk.io>
2025-04-21 20:19:05 +0200

    Add callstack

--- cardano-cli/src/Cardano/CLI/CallStackError.hs
+++ cardano-cli/src/Cardano/CLI/CallStackError.hs
@@ -1,11 +1,74 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE InstanceSigs #-}
+
 module Cardano.CLI.CallStackError where
 
-import Cardano.Api
+import Cardano.Api hiding (Error, prettyError)
 
 import Data.Bifunctor
 import Data.Text qualified as Text
+import GHC.Stack (CallStack, HasCallStack, callStack, prettyCallStack)
 import Prettyprinter
 import Prettyprinter.Render.Text
+
+-- * New definitions *
+
+class ErrorContent e where
+  prettyErrorContent :: e -> Doc ann
+
+data Content = forall e. ErrorContent e => Content e
+
+data Cause = forall c. Error c => Cause c
+
+class Error e where
+  getErrorContent :: e -> Content
+  getErrorCallStack :: e -> CallStack
+  getCause :: e -> Maybe Cause
+
+prettyErrorWithoutStack :: Error e => e -> Doc ann
+prettyErrorWithoutStack e =
+  case getErrorContent e of
+    Content content -> prettyErrorContent content
+
+prettyError :: Error e => e -> Doc ann
+prettyError e =
+  vsep
+    [ prettyErrorWithoutStack e
+    , indent 2 $ pretty $ prettyCallStack (getErrorCallStack e)
+    , ""
+    , case getCause e of
+        Nothing -> mempty
+        Just (Cause cause) ->
+          vsep
+            [ "Caused by:"
+            , indent 2 $ prettyError cause
+            ]
+    ]
+
+data ErrorWithStack e
+  = ErrorContent e => RootErrorWithStack e CallStack
+  | forall c. (ErrorContent e, Error c) => CausedErrorWithStack e CallStack c
+
+mkError :: (ErrorContent e, HasCallStack) => e -> ErrorWithStack e
+mkError e = RootErrorWithStack e callStack
+
+wrapError
+  :: HasCallStack => ErrorContent e1 => (e2 -> e1) -> ErrorWithStack e2 -> ErrorWithStack e1
+wrapError f se@(RootErrorWithStack e _) = CausedErrorWithStack (f e) callStack se
+wrapError f se@(CausedErrorWithStack e _ _) = CausedErrorWithStack (f e) callStack se
+
+instance ErrorContent e => Error (ErrorWithStack e) where
+  getErrorContent :: ErrorWithStack e -> Content
+  getErrorContent (RootErrorWithStack e _) = Content e
+  getErrorContent (CausedErrorWithStack e _ _) = Content e
+
+  getErrorCallStack :: ErrorWithStack e -> CallStack
+  getErrorCallStack (RootErrorWithStack _ cs) = cs
+  getErrorCallStack (CausedErrorWithStack _ cs _) = cs
+
+  getCause :: ErrorWithStack e -> Maybe Cause
+  getCause (RootErrorWithStack _ _) = Nothing
+  getCause (CausedErrorWithStack _ _ c) = Just $ Cause c
 
 -- * Example error definitions *
 
@@ -14,30 +77,30 @@ data ErrorA
   | ErrorAWrapper ActualErrorThrown
   deriving Show
 
-instance Error ErrorA where
-  prettyError BadErrorA = "A Bad Error A has occurred!"
-  prettyError (ErrorAWrapper e) = "A nested error of type A Wrapper has occurred, these are the details: " <> prettyError e
+instance ErrorContent ErrorA where
+  prettyErrorContent BadErrorA = "A Bad Error A has occurred!"
+  prettyErrorContent (ErrorAWrapper e) = "A nested error of type A Wrapper has occurred, these are the details: " <> prettyErrorContent e
 
 data ActualErrorThrown = ActualErrorThrown
   deriving Show
 
-instance Error ActualErrorThrown where
-  prettyError ActualErrorThrown = "An Actual Error Thrown!"
+instance ErrorContent ActualErrorThrown where
+  prettyErrorContent ActualErrorThrown = "An Actual Error Thrown!"
 
 -- * Example API *
 
-currentApiExampleFunction :: Either ErrorA ()
+currentApiExampleFunction :: Either (ErrorWithStack ErrorA) ()
 currentApiExampleFunction =
   let err = ErrorAWrapper ActualErrorThrown
-   in Left err
+   in Left (mkError err)
 
 -- * Example 1 *
 
-updatedApiExampleFunction :: Either ErrorA ()
-updatedApiExampleFunction = first ErrorAWrapper errorThrowingFunction
+updatedApiExampleFunction :: Either (ErrorWithStack ErrorA) ()
+updatedApiExampleFunction = first (wrapError ErrorAWrapper) errorThrowingFunction
 
-errorThrowingFunction :: Either ActualErrorThrown ()
-errorThrowingFunction = Left ActualErrorThrown
+errorThrowingFunction :: Either (ErrorWithStack ActualErrorThrown) ()
+errorThrowingFunction = Left (mkError ActualErrorThrown)
 
 runExample :: IO ()
 runExample = putStrLn $
@@ -47,11 +110,11 @@ runExample = putStrLn $
 
 -- * Example 2 *
 
-updatedApiExampleFunction2 :: Either ErrorA ()
+updatedApiExampleFunction2 :: Either (ErrorWithStack ErrorA) ()
 updatedApiExampleFunction2 = errorThrowingFunction2
 
-errorThrowingFunction2 :: Either ErrorA ()
-errorThrowingFunction2 = Left BadErrorA
+errorThrowingFunction2 :: Either (ErrorWithStack ErrorA) ()
+errorThrowingFunction2 = Left (mkError BadErrorA)
 
 runExample2 :: IO ()
 runExample2 = putStrLn $
