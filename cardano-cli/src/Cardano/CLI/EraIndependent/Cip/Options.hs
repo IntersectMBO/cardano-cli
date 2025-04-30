52c17e19653d59ad09776ebb20dd0d06c7396c4b
Jordan Millar <jordan.millar@iohk.io>
2025-04-30 10:20:26 -0400

    Add parsing options for cip-129

---
+++ cardano-cli/src/Cardano/CLI/EraIndependent/Cip/Options.hs
@@ -0,0 +1,32 @@
+module Cardano.CLI.EraIndependent.Cip.Options
+  ( parseCipCmd
+  )
+where
+
+import Cardano.CLI.Command (ClientCommand (CipFormatCmds))
+import Cardano.CLI.EraIndependent.Cip.Cip129.Options
+import Cardano.CLI.EraIndependent.Cip.Command
+import Cardano.CLI.Parser
+
+import Options.Applicative qualified as Opt
+
+parseCipCmd :: Opt.Parser ClientCommand
+parseCipCmd =
+  Opt.hsubparser $
+    commandWithMetavar "cip-format" $
+      Opt.info (CipFormatCmds <$> pCipFormat) $
+        Opt.progDesc $
+          mconcat
+            [ "Group of commands related to CIP changes."
+            ]
+
+pCipFormat :: Opt.Parser CipFormatCmds
+pCipFormat =
+  Opt.hsubparser $
+    commandWithMetavar "cip-129" $
+      Opt.info pCip129 $
+        Opt.progDesc $
+          mconcat
+            [ "Modified binary encoding of drep keys, constitutional committee cold and hot keys, governance actions."
+            , "https://github.com/cardano-foundation/CIPs/tree/master/CIP-0129"
+            ]
