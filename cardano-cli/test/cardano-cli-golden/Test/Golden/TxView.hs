92f433be12fcf68a20c7ebd9193b4310d97b8940
Pablo Lamela <pablo.lamela@iohk.io>
2025-05-02 18:39:06 +0200

    Add native script

--- cardano-cli/test/cardano-cli-golden/Test/Golden/TxView.hs
+++ cardano-cli/test/cardano-cli-golden/Test/Golden/TxView.hs
@@ -42,6 +42,11 @@ hprop_golden_view_babbage_yaml =
           , "42"
           , "--tx-in-execution-units"
           , "(100,110)"
+          , -- Txin with native script
+            "--tx-in"
+          , "fe5dd07fb576bff960d6e066eade5b26cdb5afebe29f76ea58d0a098bce5d891#136"
+          , "--tx-in-script-file"
+          , inputDir </> "NativeScript.json"
           , "--tx-out"
           , mconcat
               [ "addr_test1"
