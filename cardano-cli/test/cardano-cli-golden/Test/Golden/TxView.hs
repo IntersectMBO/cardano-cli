216cb989d418cb449c44575f55b0a1e0118e6013
Pablo Lamela <pablo.lamela@iohk.io>
2025-05-02 18:48:52 +0200

    Add read only ref input

--- cardano-cli/test/cardano-cli-golden/Test/Golden/TxView.hs
+++ cardano-cli/test/cardano-cli-golden/Test/Golden/TxView.hs
@@ -52,6 +52,9 @@ hprop_golden_view_babbage_yaml =
           , "fe5dd07fb576bff960d6e066eade5b26cdb5afebe29f76ea58d0a098bce5d891#137"
           , "--simple-script-tx-in-reference"
           , "fe5dd07fb576bff960d6e066eade5b26cdb5afebe29f76ea58d0a098bce5d891#138"
+          , -- Read only ref
+            "--read-only-tx-in-reference"
+          , "fe5dd07fb576bff960d6e066eade5b26cdb5afebe29f76ea58d0a098bce5d891#139"
           , -- Tx Outputs
             "--tx-out"
           , mconcat
