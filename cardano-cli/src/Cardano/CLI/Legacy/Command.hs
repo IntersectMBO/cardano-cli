358e1cfbb24313aeaafc483e9a9f203ae7f2ec0c
Jordan Millar <jordan.millar@iohk.io>
2025-05-27 16:05:33 -0400

    Remove dead code

--- cardano-cli/src/Cardano/CLI/Legacy/Command.hs
+++ cardano-cli/src/Cardano/CLI/Legacy/Command.hs
@@ -7,17 +7,13 @@ module Cardano.CLI.Legacy.Command
 where
 
 import Cardano.CLI.Legacy.Genesis.Command
-import Cardano.CLI.Legacy.Governance.Command
 
 import Data.Text (Text)
 
 data LegacyCmds
   = LegacyGenesisCmds LegacyGenesisCmds
-  | LegacyGovernanceCmds LegacyGovernanceCmds
 
 renderLegacyCommand :: LegacyCmds -> Text
 renderLegacyCommand = \case
-  LegacyGovernanceCmds cmd ->
-    renderLegacyGovernanceCmds cmd
   LegacyGenesisCmds cmd ->
     renderLegacyGenesisCmds cmd
