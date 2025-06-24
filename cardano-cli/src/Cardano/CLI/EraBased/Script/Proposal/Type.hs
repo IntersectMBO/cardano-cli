0a2b0133a5ed219ba4154d404677c60bdbba00b3
Jordan Millar <jordan.millar@iohk.io>
2025-06-24 16:10:05 -0400

    Modify proposal script witness to use ScriptRequirements

--- cardano-cli/src/Cardano/CLI/EraBased/Script/Proposal/Type.hs
+++ cardano-cli/src/Cardano/CLI/EraBased/Script/Proposal/Type.hs
@@ -3,9 +3,7 @@
 {-# LANGUAGE GADTs #-}
 
 module Cardano.CLI.EraBased.Script.Proposal.Type
-  ( CliProposalScriptRequirements (..)
-  , PlutusRefScriptCliArgs (..)
-  , PlutusScriptCliArgs (..)
+  ( PlutusRefScriptCliArgs (..)
   , ProposalScriptWitness (..)
   , createSimpleOrPlutusScriptFromCliArgs
   , createPlutusReferenceScriptFromCliArgs
@@ -13,51 +11,31 @@ module Cardano.CLI.EraBased.Script.Proposal.Type
 where
 
 import Cardano.Api
+import Cardano.Api.Experimental
+import Cardano.Api.Experimental qualified as Exp
 
+import Cardano.CLI.EraBased.Script.Type
 import Cardano.CLI.Type.Common (ScriptDataOrFile)
 
 newtype ProposalScriptWitness era
   = ProposalScriptWitness {pswScriptWitness :: ScriptWitness WitCtxStake era}
   deriving Show
 
-data CliProposalScriptRequirements
-  = OnDiskPlutusScript PlutusScriptCliArgs
-  | OnDiskSimpleScript (File ScriptInAnyLang In)
-  | OnDiskPlutusRefScript PlutusRefScriptCliArgs
-  deriving Show
-
-data PlutusScriptCliArgs
-  = OnDiskPlutusScriptCliArgs
-      (File ScriptInAnyLang In)
-      ScriptDataOrFile
-      -- ^ Redeemer
-      ExecutionUnits
-  deriving Show
-
 createSimpleOrPlutusScriptFromCliArgs
   :: File ScriptInAnyLang In
   -> Maybe (ScriptDataOrFile, ExecutionUnits)
-  -> CliProposalScriptRequirements
+  -> ScriptRequirements ProposalItem
 createSimpleOrPlutusScriptFromCliArgs scriptFp (Just (redeemer, execUnits)) =
-  OnDiskPlutusScript $ OnDiskPlutusScriptCliArgs scriptFp redeemer execUnits
+  OnDiskPlutusScript $ OnDiskPlutusScriptCliArgs scriptFp Exp.NoScriptDatumAllowed redeemer execUnits
 createSimpleOrPlutusScriptFromCliArgs scriptFp Nothing =
   OnDiskSimpleScript scriptFp
-
-data PlutusRefScriptCliArgs
-  = PlutusRefScriptCliArgs
-      TxIn
-      -- ^ TxIn with reference script
-      AnyPlutusScriptVersion
-      ScriptDataOrFile
-      -- ^ Redeemer
-      ExecutionUnits
-  deriving Show
 
 createPlutusReferenceScriptFromCliArgs
   :: TxIn
   -> AnyPlutusScriptVersion
   -> ScriptDataOrFile
   -> ExecutionUnits
-  -> CliProposalScriptRequirements
+  -> ScriptRequirements ProposalItem
 createPlutusReferenceScriptFromCliArgs txIn version redeemer execUnits =
-  OnDiskPlutusRefScript $ PlutusRefScriptCliArgs txIn version redeemer execUnits
+  PlutusReferenceScript $
+    PlutusRefScriptCliArgs txIn version Exp.NoScriptDatumAllowed NoPolicyId redeemer execUnits
