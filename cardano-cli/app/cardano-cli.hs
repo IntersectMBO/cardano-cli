959aec6c57024804197badc4d48c60835c6a3123
Jordan Millar <jordan.millar@iohk.io>
2025-07-22 15:10:09 -0400

    Restore command in error output

--- cardano-cli/app/cardano-cli.hs
+++ cardano-cli/app/cardano-cli.hs
@@ -9,6 +9,19 @@
 import           Cardano.CLI.Environment (getEnvCli)
 import           Cardano.CLI.Option (opts, pref)
 import           Cardano.CLI.Run (runClientCommand)
+import           Cardano.Api
+import           Cardano.CLI.Compatible.Exception
+import           Cardano.CLI.Command
+import           Cardano.CLI.Compatible.Command
+import           Cardano.CLI.Render
+import           Cardano.CLI.EraBased.Command
+import           Cardano.CLI.EraIndependent.Address.Command
+import           Cardano.CLI.EraIndependent.Hash.Command (renderHashCmds)
+import           Cardano.CLI.EraIndependent.Key.Command (renderKeyCmds)
+import           Cardano.CLI.EraIndependent.Node.Command (renderNodeCmds)
+import           Cardano.CLI.EraBased.Query.Command (renderQueryCmds)
+import           Cardano.CLI.Legacy.Command (renderLegacyCommand)
+import           Cardano.CLI.EraIndependent.Cip.Command (renderCipFormatCmds)
 import           Cardano.CLI.TopHandler
 import qualified Cardano.Crypto.Init as Crypto
 
@@ -17,10 +30,12 @@ import qualified Options.Applicative as Opt
 import           RIO
 
 #ifdef UNIX
-import           Cardano.CLI.OS.Posix
+import           Cardano.CLI.OS.Posix (setFileCreationMask, groupModes, otherModes, unionFileModes)
 #endif
 
 import           System.Console.Terminal.Size (Window(..), size)
+import           System.IO (hPutStrLn) 
+
 
 main :: IO ()
 main = toplevelExceptionHandler $ do
@@ -40,8 +55,25 @@ main = toplevelExceptionHandler $ do
 
   co <- Opt.customExecParser dynamicPrefs (opts envCli)
 
-  runRIO () (runClientCommand co) `catch` 
-    (\(e :: SomeException) -> do 
-      hPrint stderr $ show e
+  runRIO co (runClientCommand co) `catch` 
+    (\(e :: CustomCliException) -> do 
+      let commandText = case co of 
+            AnyEraCommand anyEraCommand  -> renderAnyEraCommand anyEraCommand 
+            AddressCommand addrCmds  -> renderAddressCmds addrCmds 
+            ByronCommand{}  -> "" 
+            CompatibleCommands anyCompatCmd -> renderAnyCompatibleCommand  anyCompatCmd
+            HashCmds hashCmds  -> renderHashCmds hashCmds 
+            KeyCommands keyCmds  -> renderKeyCmds keyCmds 
+            NodeCommands nodeCmds  -> renderNodeCmds nodeCmds 
+            QueryCommands queryCmds  -> renderQueryCmds queryCmds 
+            LegacyCmds legacyCmds  -> renderLegacyCommand legacyCmds 
+            CipFormatCmds cipFmtCmds  -> renderCipFormatCmds cipFmtCmds 
+            CliPingCommand{}  -> ""
+            CliDebugCmds{} -> "" 
+            Help{} -> "" 
+            DisplayVersion -> "" 
+
+      hPutStrLn stderr $ docToString $ renderAnyCmdError commandText prettyException e
+
       exitWith $ ExitFailure 1
     )
