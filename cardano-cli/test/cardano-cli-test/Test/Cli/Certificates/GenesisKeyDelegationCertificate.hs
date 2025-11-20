fc6b6b219520da98439cd9e5011b086e8c0ac60b
Jordan Millar <jordan.millar@iohk.io>
2025-11-20 09:38:41 -0400

    The genesis key delegation certificate test was never a golden test Convert this test to a simple certificate decoding test and move the test to cardano-cli-test

--- cardano-cli/test/cardano-cli-test/Test/Cli/Certificates/GenesisKeyDelegationCertificate.hs
+++ cardano-cli/test/cardano-cli-test/Test/Cli/Certificates/GenesisKeyDelegationCertificate.hs
@@ -1,23 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-module Test.Golden.Shelley.TextEnvelope.Certificates.GenesisKeyDelegation where
+module Test.Cli.Certificates.GenesisKeyDelegationCertificate where
+
+import Cardano.Api (File (..), ShelleyLedgerEra, readFileTextEnvelope)
+import Cardano.Api.Experimental as Exp
 
 import Control.Monad (void)
 
 import Test.Cardano.CLI.Util
 
-import Hedgehog (Property)
+import Hedgehog (Property, evalIO, success)
 import Hedgehog.Extras qualified as H
+import Hedgehog.Internal.Property (failWith)
 
 hprop_golden_shelleyGenesisKeyDelegationCertificate :: Property
 hprop_golden_shelleyGenesisKeyDelegationCertificate =
   watchdogProp . propertyOnce . H.moduleWorkspace "tmp" $ \tempDir -> do
-    -- Reference certificate
-    referenceCertificateFilePath <-
-      noteInputFile $
-        "test/cardano-cli-golden/files/input/shelley/certificates/"
-          <> "genesis_key_delegation_certificate"
-
     -- Verification key and certificate filepaths
     genesisVerKeyFilePath <-
       noteTempFile tempDir "genesis-verification-key-file"
@@ -96,5 +95,8 @@ hprop_golden_shelleyGenesisKeyDelegationCertificate =
         ]
 
     H.assertFilesExist [genesisKeyDelegCertFilePath]
-
-    H.diffFileVsGoldenFile genesisKeyDelegCertFilePath referenceCertificateFilePath
+    eCert <-
+      evalIO $ readFileTextEnvelope $ File genesisKeyDelegCertFilePath
+    case eCert of
+      Left err -> failWith Nothing $ "Failed to decode generated genesis key delegation certificate: " <> show err
+      Right (_cert :: Exp.Certificate (ShelleyLedgerEra BabbageEra)) -> success
