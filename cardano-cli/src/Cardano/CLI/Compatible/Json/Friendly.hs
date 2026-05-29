b28efce6591ef07b0b866a4f13bee3bd1b6e4f37
John Lotoski <john.lotoski@iohk.io>
2026-05-29 17:19:20 -0500

    Render Dijkstra-era txs in debug transaction view

--- cardano-cli/src/Cardano/CLI/Compatible/Json/Friendly.hs
+++ cardano-cli/src/Cardano/CLI/Compatible/Json/Friendly.hs
@@ -31,7 +31,7 @@ module Cardano.CLI.Compatible.Json.Friendly
 where
 
 import Cardano.Api as Api
-import Cardano.Api.Experimental (obtainCommonConstraints)
+import Cardano.Api.Experimental (conwayEraOnwardsCommonConstraints, obtainCommonConstraints)
 import Cardano.Api.Experimental qualified as Exp
 import Cardano.Api.Ledger (ExUnits (..), extractHash, strictMaybeToMaybe)
 import Cardano.Api.Ledger qualified as Alonzo
@@ -197,11 +197,11 @@ friendlyTxBodyImpl sbe tb = do
             sbe
             mempty
             ( \cOnwards ->
-                conwayEraOnwardsConstraints cOnwards $
-                  case txProposalProcedures of
-                    Nothing -> []
-                    Just (Featured _ TxProposalProceduresNone) -> []
-                    Just (Featured _ pp) -> do
+                case txProposalProcedures of
+                  Nothing -> []
+                  Just (Featured _ TxProposalProceduresNone) -> []
+                  Just (Featured _ pp) ->
+                    conwayEraOnwardsCommonConstraints cOnwards $ do
                       let lProposals = toList $ convProposalProcedures pp
                       ["governance actions" .= friendlyLedgerProposals (convert cOnwards) lProposals]
             )
@@ -262,7 +262,7 @@ friendlyLedgerProposal proposalProcedure = object $ friendlyProposalImpl (Propos
 
 friendlyVotingProcedures
   :: ConwayEraOnwards era -> L.VotingProcedures (ShelleyLedgerEra era) -> Aeson.Value
-friendlyVotingProcedures cOnwards x = conwayEraOnwardsConstraints cOnwards $ toJSON x
+friendlyVotingProcedures cOnwards x = conwayEraOnwardsCommonConstraints cOnwards $ toJSON x
 
 data EraIndependentPlutusScriptPurpose
   = Spending
